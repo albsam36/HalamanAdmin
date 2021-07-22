15ca25a586dce50ff2a7eb441f636dd38294fe42
Kinaraalg <Kinaraalghifari17@gmail.com>
2021-07-22 07:59:46 +0700

    Install Firebase + Setup

--- admin-analitica-sites/src/main.js
+++ admin-analitica-sites/src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -10,3 +11,14 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+let config = {
+  apiKey: "AIzaSyBxPemPIXRlqExrVdnnTxOPvJqD9b0Sw6A",
+  authDomain: "analitica-dev2.firebaseapp.com",
+  databaseURL: "https://analitica-dev2.firebaseio.com",
+  projectId: "analitica-dev2",
+  storageBucket: "analitica-dev2.appspot.com",
+  messagingSenderId: "811817140779",
+  appId: "1:811817140779:web:ee2c05eb21304736f5206e"
+};
+firebase.initializeApp(config);
